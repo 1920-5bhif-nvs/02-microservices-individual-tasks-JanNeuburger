39dbf1b431eb63614f263eeb3fdc781889403f1c
JanNeuburger <jan.neuburger.jn@gmail.com>
2019-10-14 20:48:59 +0200

    initialized project

---
+++ project-manager/src/test/java/at/htl/rest/NativeProjectResourceIT.java
@@ -0,0 +1,9 @@
+package at.htl.rest;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeProjectResourceIT extends ProjectResourceTest {
+
+    // Execute the same tests but in native mode.
+}
